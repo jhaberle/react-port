de6f2676ca0d9a0e97dbf742f945e544d58c9a9e
Jake Haberle <59548398+jhaberle@users.noreply.github.com>
2020-11-30 00:00:24 -0700

    update and final changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+
+  width: 100%;
+`;
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="row">
-        <div class="col-sm-12">
-          <footer>&copy Jake Haberle - 2020</footer>
+    <StyledFooter>
+      <div className="footer">
+        <div className="row">
+          <div class="col-sm-12">
+            <footer>Jake Haberle - 2020</footer>
+          </div>
         </div>
       </div>
-    </div>
+    </StyledFooter>
   );
 };
 
